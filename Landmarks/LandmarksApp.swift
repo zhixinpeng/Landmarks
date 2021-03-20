b6f7095b5dde2ad5fce7bd92c2a3df8d341b919e
彭智鑫 <pengzhixin@lizhi.fm>
2021-03-20 19:21:12 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 彭智鑫 on 2021/3/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
